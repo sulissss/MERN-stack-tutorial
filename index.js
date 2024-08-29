3b5f1508d537a2ce8035cc0f1692df24636b83b3
sulissss <sul434.ahmad@gmail.com>
2024-08-29 20:08:09 +0500

    before using express

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-// const http = require('http');
-const fs = require('fs');
-const express = require('express');
+const http = require('http');
 
-const myText = fs.readFile('sample.txt', 'utf-8', (err, txt) => {
-    console.log(txt);
-});
+console.log('yoo');
+
+data = {'age': 32};
 
 const server = http.createServer((req, res) => {
-    
+    if (req.url === '/') {
+        res.end(`${req.method}`);
+    }
+    else if (req.url === '/demo') {
+        res.end('demooooooo');
+    }
+    else {
+        console.log('server started');
+        res.setHeader('Content-Type', 'application/json');
+        // res.end('<h2>hi brother</h2>');
+        res.end(JSON.stringify(data));
+    }
 })
+
+server.listen(8080);
