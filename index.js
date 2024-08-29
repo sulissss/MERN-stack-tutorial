66942d917e97873b0764d21fe0926afc63d0dc54
sulissss <sul434.ahmad@gmail.com>
2024-08-29 17:45:33 +0500

    chap 2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+// const http = require('http');
+const fs = require('fs');
+const express = require('express');
+
+const myText = fs.readFile('sample.txt', 'utf-8', (err, txt) => {
+    console.log(txt);
+});
+
+const server = http.createServer((req, res) => {
+    
+})
