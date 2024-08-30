a4fbb0c3d134b01cdddcf11e2d8b5db8989f9c53
sulissss <sul434.ahmad@gmail.com>
2024-08-30 05:57:29 +0500

    Chapter 7 - Mongoose

--- index.js
+++ index.js
@@ -13,26 +13,8 @@ async function main() {
     console.log('database connected');
 }
 
-// const taskSchema = new Schema({
-//     title: {type: String, required: true, unique: true},
-//     status: Boolean,
-//     date: {type: Date, default: Date.now}
-// })
-
-// const Task = mongoose.model('Task', taskSchema);
-
-// server.post('/task/:title', (req, res) => {
-//     const newTask = new Task({
-//         title: req.params.title,
-//         status: true
-//     });
-//     newTask.save();
-//     res.send('Task saved succssfully!');
-// })
-
 const productRouter = require('./routes/product-route');
 server.use('/products', productRouter.router);
-
 
 server.listen(8080, () => {
     console.log('server started');
